bbc3b42351badf44e52def1f6e21b81040fb3366
piyusss11 <piyusss11@gmail.com>
2024-09-18 13:19:19 +0530

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Canvas } from "@react-three/fiber";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+type CubeProps = {
+  posi: [number, number, number];
+  color: string;
+  scale: [number, number, number];
+};
+const Cube = ({ posi, color, scale }: CubeProps) => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <mesh position={posi}>
+      <boxGeometry args={scale} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
+};
+function App() {
+  return (
+    <Canvas>
+      <directionalLight position={[0, 0, 3]} intensity={1} />
+      <ambientLight intensity={1} />
+      {/* <directionalLight position={[1,1,10]}/> */}
+      <group position={[0, -1, 0]}>
+        <Cube posi={[-1, 2, 0]} color="green" scale={[1, 1, 1]} />
+        <Cube posi={[1, 2, 0]} color="red" scale={[1, 1, 1]} />
+        <Cube posi={[-1, 0, 0]} color="blue" scale={[1, 1, 1]} />
+        <Cube posi={[1, 0, 0]} color="hotpink" scale={[1, 1, 1]} />
+      </group>
+    </Canvas>
+  );
 }
 
-export default App
+export default App;
